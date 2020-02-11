4d27f34d4c567490004e533301ef950d5490acca
Akalin <a.f.akalin@gmail.com>
2020-02-11 13:51:35 +0100

    user commponents created

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1> Hello {{name}}</h1>`,
+  template: `<h1> Hello {{name}}</h1>
+  <p><strong>Email: {{email}}</strong></p>
+  <p><strong>Address: {{address.city}} {{address.street}}</strong></p>
+  `,
 })
 export class AppComponent  { 
-    name = "Ali";
+    name = "Ali Akalin";
+    email = "pimpip@pim.pip"
+    address ={
+      street: "MAria Tusch Str",
+      city: "Vienna"
+    }
 }
--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -1,59 +1,17 @@
 import { Component } from '@angular/core';
-import {PostsService} from '../services/posts.service';
 
 @Component({
-    moduleId: module.id,
-    selector: 'user',
-    templateUrl: 'user.component.html',
-    providers: [PostsService]
+  selector: 'my-app',
+  template: `<h1> Hello {{name}}</h1>
+  <p><strong>Email: {{email}}</strong></p>
+  <p><strong>Address: {{address.city}} {{address.street}}</strong></p>
+  `,
 })
-export class UserComponent  { 
-  name: string; 
-  email: string;
-  address: address;
-  hobbies: string[];
-  showHobbies: boolean;
-  posts:Post[];
-
-  constructor(private postsService: PostsService){
-    this.name = 'John Doe'; 
-    this.email = 'john@gmail.com',
-    this.address = {
-        street: '12 Main st',
-        city: 'Boston', 
-        state: 'MA'
+export class AppComponent  { 
+    name = "Ali Akalin";
+    email = "pimpip@pim.pip"
+    address ={
+      street: "MAria Tusch Str",
+      city: "Vienna"
     }
-    this.hobbies = ['Music', 'Movies', 'Sports'];
-    this.showHobbies = false;
-
-    
-    this.postsService.getPosts().subscribe(posts => {
-        this.posts = posts;
-    });
-  }
-
-
-  toggleHobbies(){
-      this.showHobbies = !this.showHobbies;
-  }
-
-  addHobby(hobby: any){
-      this.hobbies.push(hobby);
-  }
-
-  deleteHobby(i: any){
-      this.hobbies.splice(i, 1);
-  }
-}
-
-interface address {
-    street: string;
-    city: string;
-    state: string;
-}
-
-interface Post{
-    id: number;
-    title: string;
-    body: string;
 }
