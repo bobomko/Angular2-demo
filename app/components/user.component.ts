c77e868dfffe2561bb9c0af4d13126f794a64f22
Akalin <a.f.akalin@gmail.com>
2020-02-11 14:12:57 +0100

    *ngIf="showHobbies"

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -5,17 +5,36 @@ import { Component } from '@angular/core';
   template: `<h1> Hello {{name}}</h1>
   <p><strong>Email: {{email}}</strong></p>
   <p><strong>Address: {{address.city}} {{address.street}}</strong></p>
+  <div *ngIf="showHobbies">
+    <h3>Hobbies</h3>
+    <ul>
+      <li *ngFor="let hobby of hobbies">
+      {{hobby}}
+      </li>
+    </ul>
+  </div>
   `,
 })
 export class UserComponent  { 
-    name = "Ali Akalin";
-    email = "pimpip@pim.pip"
-    address ={
-      street: "MAria Tusch Str",
-      city: "Vienna"
-    }
-    
+    name: string;
+    email: string;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+
     constructor(){
-        this.name = 'Sami Abe';
-      }
+        this.name = 'Ali Akalin';
+        this.email = 'pimpip@pim.pip';
+        this.address ={
+          street: 'MAria Tusch Str',
+          city: 'Vienna'
+        } 
+        this.hobbies = ['Music', 'Movies','Sports'];
+        this.showHobbies = false;
+    }
 }
+interface address{
+    street: string;
+    city: string;
+}
+
