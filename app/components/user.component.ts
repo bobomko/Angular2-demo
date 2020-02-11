fbdf090e7929b0b89f8d12391ec5a46e46db22bf
Akalin <a.f.akalin@gmail.com>
2020-02-11 16:04:09 +0100

    ctamplate html created and extracted

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -3,42 +3,9 @@ import {PostService} from '../services/post.service';
 import { post } from 'selenium-webdriver/http';
 
 @Component({
+    moduleId: module.id,
   selector: 'user',
-  template: `<h1>{{name}}</h1>
-  <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.city}}, {{address.street}}</p>
-  <button (click)="toggleHobbies()">{{showHobbies? "Hide Hobbies" : "Schow Hobbies"}}</button>
-  <div *ngIf="showHobbies">
-    <h3>Hobbies</h3>
-    <ul>
-      <li *ngFor="let hobby of hobbies; let i = index">
-      {{hobby}} <button (click) = "deleteHobby(i)">X</button>
-      </li>
-        <form (submit) = "addHobby(hobby.value)">
-            <label>Add Hobby: </label><br />
-            <input type="text" #hobby /><br />
-        </form>    
-    </ul>
-  </div>
-  <hr/>
-  <h3>Edit User</h3>
-  <form>
-  <label>Name: </label><br />
-  <input type="text" name= "name" [(ngModel)]="name" /><br />
-  <label>Email: </label><br />
-  <input type="text" name= "email" [(ngModel)]="email" /><br />
-  <label>City: </label><br />
-  <input type="text" name= "address.city" [(ngModel)]="address.city" /><br />
-  <label>Street: </label><br />
-  <input type="text" name= "address.street" [(ngModel)]="address.street" /><br />
-  </form>
-  <hr />
-  <h3>Posts</h3>
-  <div *ngFor="let post of posts">
-    <h3>{{post.title}}</h3>
-    <p>{{post.body}}</p>
-    </div>
-  `,
+  templateUrl: 'user.component.html',
   providers: [PostService]
 })
 export class UserComponent  { 
